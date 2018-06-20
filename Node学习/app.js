20d176b43f8fd91351330ceb883a2a194b7e62d7
gaoxiaoming <gxm@heanny.cn>
2018-06-20 11:13:39 +0800

    'node学习'

--- Node学习/app.js
+++ Node学习/app.js
@@ -1,8 +1,28 @@
+// node创建一个服务器(方法一)
 let http = require('http');
 
 http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.end('hello world!')
-}).listen(8888)
+}).listen(8888);
 
-console.log('Server running at http://127.0.0.1:8888') 
+console.log('Server running at http://127.0.0.1:8888');
+
+
+// node创建一个服务器(方法二)
+/*
+const http = require('http');   //加载模块
+
+const hostname = '127.0.0.1';   //地址
+const port = 3000;              //端口
+
+const server = http.createServer((req, res) => {        //创建服务器，成功回调
+    //req请求，res响应
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {       //监听
+    console.log(`Server running at http://${hostname}:${port}/`);
+});*/
---
+++ Node学习/北森学习网-进击node.md
@@ -0,0 +1,25 @@
+##为什么学习node
+1. 很火
+2. 很强
+3. 运行在chrome V8 引擎上的JavaScript的运行环境
+4. 没有浏览器端的安全限制  
+
+##推荐网站
+1. nodejs.org（官网）  
+2. www.npmjs.com（社区）
+3. github.com（代码托放平台）  
+4. stackoverflow.com（技术问答社区）
+
+##nodeJs版本
+1. 偶数位为稳定版本  
+    0.6.x  
+    0.8.x  
+    0.10.x
+2. 奇数位为非稳定版本  
+    0.7.x  
+    0.9.x  
+    0.11.x  
+
+
+
+
